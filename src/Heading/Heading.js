246cfb724b0fd47e6150b3971655c75a7cb0a061
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-28 09:48:36 -0800

    [add]

---
+++ src/Heading/Heading.js
@@ -0,0 +1,12 @@
+import './Heading.css';
+
+function Heading() {
+    return (
+        <div className='Heading'>
+            <h1>Sharmaine Borbe</h1>
+            <p>ACS 1320 Final Assessment</p>
+        </div>
+    )
+}
+
+export default Heading
