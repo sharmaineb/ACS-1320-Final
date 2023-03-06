65000116409e6425d3f0bc9e7f69b9fda632934c
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-03-06 13:54:12 -0800

    [update] added fan total

--- src/components/MetalMeta/MetalMeta.js
+++ src/components/MetalMeta/MetalMeta.js
@@ -3,10 +3,14 @@ import data from '../../metal.json'
 
 function MetalMeta() {
     const numBands = data.length;
+    const numFans = data.reduce((num, band) =>{
+      return num + band.fans;
+    }, 0)
   
     return (
       <div className="bands">
         <p><strong>🤘Total Bands:</strong> {numBands}</p>
+        <p><strong>🤘Total Fans:</strong> {(numFans*1000).toLocaleString('en-US')}</p>
       </div>
     );
   }
